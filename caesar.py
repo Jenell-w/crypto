f8a5cd748ec34bedc3137f65531592f8f5cbc05c
Jenell-w <49175487+Jenell-w@users.noreply.github.com>
2019-05-20 14:53:14 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,5 +1,9 @@
 from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
-    #Add your code here
-    return
+    result = ""
+    for char in text:
+        char_idx = rotate_character(char, rot)
+        result += char_idx      
+    return result
+    
