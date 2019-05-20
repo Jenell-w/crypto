3cda9e4d6614be3499df506c0de45e56e2bb3884
Jenell-w <49175487+Jenell-w@users.noreply.github.com>
2019-05-20 14:37:46 -0600

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,10 +1,25 @@
-
-
 def alphabet_position(letter):
-    #Add your code here
-    return
-
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    if letter.isupper():
+        letter = letter.lower()
+        
+    result = alphabet.index(letter)    
+    return result 
 
 def rotate_character(char, rot):
-    #Add your code here
-    return
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    result = ""
+    
+    for letters in char:
+        if char not in alphabet:
+            if char not in alphabet2:
+                return char
+        rot_ind = (alphabet_position(char) + rot) % 26
+        if letters in alphabet2:
+            result += alphabet2[rot_ind]
+            result.upper()
+            continue
+        else:
+            result += alphabet[rot_ind]       
+    return result   
