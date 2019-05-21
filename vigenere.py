a4899b9d6eab7dae3d97229e02a75f01ab9903f6
Jenell-w <49175487+Jenell-w@users.noreply.github.com>
2019-05-20 19:32:42 -0600

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,7 +1,18 @@
 from helpers import alphabet_position, rotate_character
 
-def encrypt(text, key):
-    #Add your code here
-    return
+def encrypt(message, key):
+    result = ""
+    key_index = 0
+
+    for char in message:
+        rot_key = key[key_index]
+        rot_amount = alphabet_position(rot_key)
+        if char.isalpha():
+            new_char = rotate_character(char, rot_amount)
+            result += new_char
+            key_index = (key_index + 1) % len(key)  
+        else:
+            result += char
+    return result
 
 
